fa8a04f6b02d6692466a41ca62c05235d1da2228
Nils Muthreich <nmuth@max-exfl265.desy.de>
2025-03-15 15:13:11 +0100

    add number of shots option

--- submit_focus_scan.sh
+++ submit_focus_scan.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 #
-#SBATCH --partition=upex
-#SBATCH --time=01:00:00
+#SBATCH --reservation=upex_006933
+#SBATCH --time=05:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=64GB
 #SBATCH --output=/gpfs/exfel/exp/SPB/202501/p006933/scratch/Logs/log_focusscan/job_%A_%a.out
 #SBATCH --error=/gpfs/exfel/exp/SPB/202501/p006933/scratch/Logs/log_focusscan/job_%A_%a.err
 #SBATCH --export=ALL
+# # #SBATCH --partition=upex
 
 # For submitting do the following
-# # # sbatch --export=R=8 submit_focus_scan.sh
+# # # sbatch --export=R=8,F=1 submit_focus_scan.sh
+# # # seq 35 99 | xargs -I{} sbatch --export=R={},F=1,N=200 submit_focus_scan.sh
 
 # Default values for optional arguments
 RUN=$R
+FLAG=$F
+NSHOT=$N
 PYTHON_SCRIPT_PATH="/gpfs/exfel/exp/SPB/202501/p006933/usr/Software/analysistools/focus_scan.py"
 
 echo $RUN
+echo $FLAG
 
 ###cd $SLURM_SUBMIT_DIR
 
@@ -25,4 +30,4 @@ source /usr/share/Modules/init/bash
 module load exfel exfel-python
 
 # Run the Python script
-python $PYTHON_SCRIPT_PATH --run=$RUN
+python $PYTHON_SCRIPT_PATH --run=$RUN --flag=$FLAG --nshot=$NSHOT
